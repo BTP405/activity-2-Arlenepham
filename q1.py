69c8f36ce131e2865c0f1597af8f892a75c4682e
Arlene <122369943+Arlenepham@users.noreply.github.com>
2024-02-13 10:37:25 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,15 @@
+# this helper function checks if the integer in the parameter is a prime number. 
+# It iterates through the loop, if it diviable for any index, it is not a prime number
+def isPrime(n):
+    prime = True
+    for i in range(2, n):
+        if (n % i) == 0:
+            prime = False
+            break
+    return prime
+
 def getPrimeNumbers(n):
+    return [i for i in range(2, n) if isPrime(i)] # this list comprehension return a list of int from 2 to n is a prime number
+
+# example
+print(getPrimeNumbers(30))
